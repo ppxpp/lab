3664eab33693c3de487ae7ad5b852020b320a67d
ppxpp <zhouhao3207@qq.com>
2014-07-03 04:04:31 +0000

    modify db.py

--- project/service/db.py
+++ project/service/db.py
@@ -150,7 +150,7 @@ class Connection(object):
 
 	def _execute(self, cursor, query, parameters):
 		try:
-			return cursor.execute(query, parameters)
+			return cursor.execute(query, [parameters])
 		except Exception:
 			print "Error connecting to MySQL on %s" %(self.host)
 			self.close()
